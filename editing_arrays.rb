11c0e92ea238720c631bfd96d6e8571ef033a6c0
ziesop20 <sincerelysophie@hotmail.com>
2018-07-30 13:51:45 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,13 +1,19 @@
-rainbow_colors = [
-  "yellow",
-  "default",
-  "light_cyan"
-]
+# rainbow_colors = ["yellow","default","light_cyan"]
+#   rainbow_colors[0] = "red"
+#   puts rainbow_colors
 
-def change_rainbow_colors
-  # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-end
+# rainbow_colors = ["yellow","default","light_cyan"]
+#   rainbow_colors[1] = "light_red"
+#   puts rainbow_colors
+  
+  # rainbow_colors = ["red","light_red","light_cyan"]
+  # rainbow_colors[2] = "light_yellow"
+  # puts rainbow_colors
+  
+  # rainbow_colors = ["red","light_red","light_yellow"]
+  # rainbow_colors.push "green"
+  # puts rainbow_colors
 
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-end
+ rainbow_colors = ["red","light_red","light_yellow", "green"]
+  rainbow_colors.push "blue"
+  puts rainbow_colors
